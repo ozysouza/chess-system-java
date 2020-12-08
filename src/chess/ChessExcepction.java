94429403d9ef10e87568c6653535b3bdd832d066
Oziel Souza <ozysouzaa@gmail.com>
2020-12-08 10:38:54 -0300

    Board Excepction, defensive programing, ChessExpection and Chess Position

---
+++ src/chess/ChessExcepction.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessExcepction extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExcepction(String msg) {
+		super(msg);
+	}
+
+}
